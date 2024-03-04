0ab4e512b3131875e6f53506451bfebabc9dd706
¨luisantonio16¨ <luis16.16m@gmail.com>
2024-03-04 12:29:25 -0400

    start

---
+++ luisWeb/src/paginas/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Home() {
+    return (
+        <>
+            <div className="container__menu-icon">
+                <article className="menu-icon">
+                    <i className='bx bx-menu-alt-right icon-menu' ></i>
+
+                </article>
+
+            </div>
+            <h1>Luis Mercedes Web</h1>
+            <p>Desarrollador de Software</p>
+        </>
+    );
+
+}
+export default Home
